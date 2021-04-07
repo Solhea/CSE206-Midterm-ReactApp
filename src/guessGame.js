b67c78eb9085d98e2625120ca844d81e9a57095d
Baran Eryürek <baran.4821@hotmail.com>
2021-04-07 22:19:03 +0300

    guessGame class

---
+++ src/guessGame.js
@@ -0,0 +1,55 @@
+class guessGame {
+    constructor() {
+        this.numberArray = this.#createNumberArray();
+    }
+
+    #createNumberArray() {
+        var numbers = [];
+        while(numbers.length < 4) {
+            var number = Math.floor(Math.random()*10);
+            if(numbers.indexOf(number) == -1) {
+                numbers.push(number.toString());
+            }
+        }
+
+        return numbers;
+    }
+
+    checkAnswer(answer) {
+        var answerArray = this.#NumbertoArray(answer);
+        var plus = 0;
+        var minus = 0;
+        var result = "";
+
+        for (let i = 0; i < answerArray.length; i++) {
+            if(this.numberArray.indexOf(answerArray[i]) != -1) {
+                if(this.numberArray.indexOf(answerArray[i]) == i) {
+                    plus++;
+                } else {
+                    minus++;
+                }
+            }
+        }
+
+        if(plus != 0) {
+            result += "+" + plus.toString();
+        }
+        if(minus != 0) {
+            result += "-" + minus.toString();
+        }
+        if(result == "") {
+            result = "0";
+        }
+
+        return result;
+    }
+
+    #ArraytoNumber(numbers) {
+        return numbers[0]+numbers[1]+numbers[2]+numbers[3];
+    }
+    #NumbertoArray(number) {
+        number = number.toString();
+
+        return number.split("");
+    }
+}
