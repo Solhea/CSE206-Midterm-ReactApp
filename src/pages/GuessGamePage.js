087e59978b7a2dbb92fe61d31ce31d3deee3824f
CNEK <cenk_akay08@hotmail.com>
2021-04-17 20:57:32 +0300

    merge

--- src/pages/GuessGamePage.js
+++ src/pages/GuessGamePage.js
@@ -1,88 +1,87 @@
-import '../assets/css/GuessGamePage.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
-import React, { Component } from 'react';
-import GuessGame from '../javaScripts/guessGame';
 
+import React, { useState, useEffect } from "react";
+import RandomNumber from "./RandomNumber";
+import CheckTheResult from "./CheckTheResult";
 
-class GuessGamePage extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-          prediction: '',
-          totalPredictions: 0,
-          result: 0,
-          isButtonDisabled: true,
-          guessGame: new GuessGame()
-        }
-      }
-
-      check_onClick=()=> {
-        this.setState({totalPredictions: this.state.totalPredictions+=1,
-            result: this.state.guessGame.checkAnswer(this.state.prediction)
-         },()=>{this.checkResult();});
-         //console.log(this.state.guessGame.number);
-      }
-      
-      checkResult=()=> {
-        if(this.state.result === "+4") {
-            alert("Congratulations! You have found: "+ this.state.guessGame.number);
-            this.setState({
-                prediction: '',
-                totalPredictions: 0,
-                result: 0,
-                isButtonDisabled: true,
-                guessGame: new GuessGame()
-            });
-        }
-      }
-
-      handleChange(evt) {
-        //This way user only can enter numbers
-        const prediction = (evt.target.validity.valid) ? evt.target.value : this.state.prediction;
-
-        this.setState({ prediction });
-
-        //This way user can't enter anything but length of 4
-        if(prediction.length === 4) {
-            this.setState({isButtonDisabled: false});
-        } else {
-            this.setState({isButtonDisabled: true});
-        }
-      }
-
-  render () {
-    return(
-        <div class="GuessGame">
-            <div>
-                <h2>Guess the Number With Four Digits</h2>
-            </div>
-            <br/>
-            <div class="ShowForm">
-               Type your guess:
-               <br/>
-               <Form.Group>
-                   <Form.Control type="text" pattern="[0-9]*" maxLength="4" onInput={this.handleChange.bind(this)} value={this.state.prediction} placeholder="Enter 4 digit number" />
-               </Form.Group>
-            </div>
-            <div class="PredicText">
-                Number of Predictions: {this.state.totalPredictions}
-            </div>
-            <div>
-                <Button variant="success" onClick={this.check_onClick} disabled={this.state.isButtonDisabled} >Check!</Button>
-            </div>
-            <div class="ShowForm">
-                Result:
-                <div class="Result"> 
-                    {this.state.result}
-                </div>
-            </div>
-        </div>
-    );
+const AlertCheck = (ReturnedResult, RandomNumber) => {
+  if (ReturnedResult === "+4") {
+    alert("Congratulations! The answer is: " + RandomNumber.toString());
   }
-}
+};
 
+const GuessGamePage = () => {
+  const [randomNumber, setRandomNumber] = useState(() => RandomNumber());
+  const [counter, setCounter] = useState(0);
+  const [result, setResult] = useState("");
 
+  const check = (userNumber) => {
+    /* console.log(document.getElementById("UserInput").value)
+    console.log(SecretNumber);
+    document.getElementById("UserInput").value = "+4"; */
+    setCounter(counter + 1);
+
+    setResult(CheckTheResult(userNumber, randomNumber));
+  };
+  const NewGameSetting = () => {
+    setCounter(0);
+    setResult("");
+    document.getElementById("UserInput").value = "";
+  };
+
+  useEffect(() => {
+    AlertCheck(result, randomNumber);
+  }, [result]);
+  useEffect(() => {
+    NewGameSetting();
+  }, [randomNumber]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div>
+          Type Your Guess
+          <input
+            type="text"
+            id="UserInput"
+            placeholder="Guess The Number"
+            name="fname"
+          ></input>
+        </div>
+        <button
+          type="button"
+          id="GuessEvent"
+          onClick={() => {
+            check(document.getElementById("UserInput").value);
+          }}
+        >
+          Click Me!{randomNumber}
+        </button>
+        <div>Number of prediction: {counter}</div>
+        <div>Result: {result}</div>
+        <button
+          type="button"
+          id="ReGuessEvent"
+          onClick={() => {
+            setRandomNumber(() => RandomNumber());
+          }}
+        >
+          Generate New Random Number
+        </button>
+      </header>
+    </div>
+  );
+};
 
 export default GuessGamePage;
