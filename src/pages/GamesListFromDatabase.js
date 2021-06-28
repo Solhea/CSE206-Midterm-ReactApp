cd1fea3e231ef64e22d260e78ec8d279bfae34c1
CNEK <cenk_akay08@hotmail.com>
2021-06-28 16:11:54 +0300

    old

--- src/pages/GamesListFromDatabase.js
+++ src/pages/GamesListFromDatabase.js
@@ -12,7 +12,7 @@ const GamesListFromDatabase = () => {
       setGames(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     fetchData();
-  }, [games]);
+  }, []);
 
   const onCreate = () => {
     const db = fire.firestore();
